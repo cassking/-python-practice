69394008fbdb4cb4cb83aa587b7bb3d3447f619b
Cassandra King <puddincat@gmail.com>
2018-06-08 16:28:01 -0400

    update methods for strings to arrays

--- lists-tuples-sets.py
+++ lists-tuples-sets.py
@@ -9,6 +9,8 @@ courses_3 =['historyxxx', 'mathxxx', 'sciencexxx', 'stuffxxx']
 
 #modifying lists
 courses_2= ['spanish', 'english']
+list= ['wooho', 'bike', 'run', 'eat', 'sleep']
+
 courses.append("art")#one value
 courses.insert(0, "dance")
 courses.insert(0, courses_2)
@@ -20,6 +22,39 @@ courses.append(courses_2)#extend vs apend
 
 courses_3.reverse()
 courses_3.sort(reverse=True)
-nums.sort(reverse=True)
+nums.sort(reverse=True)#sort method alters list
+#use instead sorted funciton
+sorted_list = sorted(list)
 print(courses_3)
 print(nums)
+print(list)
+print(sorted_list)
+print(max(nums))
+print(min(nums))
+print(sum(nums))
+print(courses.index('math'))
+print('math' in nums)
+print('math' in courses)
+#foor loops
+# for item in courses:
+#     print(item)
+#
+
+#array to string
+stringified = "   ,".join(courses_3)
+print(stringified)
+
+#string to array
+new_list = stringified.split('-')
+print(new_list)
+
+#tuples and sets
+
+#access index an dvlaue
+# for index, item in enumerate(courses, start=2):
+#     print(index,item)
+
+
+
+
+#tuples and sets
