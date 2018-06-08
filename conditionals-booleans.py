44ec0d9d654a05256269da28bf87b1bf0adcabed
Cassandra King <puddincat@gmail.com>
2018-06-08 17:50:21 -0400

    basic syntax for contitionals booleans

---
+++ conditionals-booleans.py
@@ -0,0 +1,38 @@
+
+language = 'java'
+if  True:
+    print('this is true')
+if language == 'python':
+    print('this is true for pyth')
+
+
+#OBJECT IDENTITY checks to see if same object in
+# memory
+if language == 'python':
+    print('...language is python')
+elif language == 'java':
+    print('...language is java not python')
+elif language == 'javascript':
+    print('language is js not python')
+else:
+    print('no match')
+
+
+
+user = 'admin'
+logged_in = True
+
+if user != 'admin' and logged_in:
+    print('admin page')
+else:
+    print('not allowed')
+
+
+a = [1,2,3]
+b = [1,2,3]
+print(a is b)
+#same as
+print(id(a) == id(b))
+
+print(id(a))#diff objects
+print(id(b))
